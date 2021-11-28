202ef78f6f1ea2b48c8853e4363608ce341c3b50
AllanT12 <alin.tanasa@student.usv.ro>
2021-11-28 13:04:22 +0200

    login

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalEdu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
+        public UserController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        [HttpGet("GoogleSignIn")]
+        public async Task<ActionResult> GoogleSignIn()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = "/"
+        };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+        [HttpGet("FacebookSignIn")]
+        public async Task<ActionResult> FacebookSignIn()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = "/"
+            };
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace DigitalEdu
 {
@@ -25,6 +30,19 @@ namespace DigitalEdu
         }
 
         public IConfiguration Configuration { get; }
+
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                // TODO: Use your User Agent library of choice here.
+                
+                    // For .NET Core < 3.1 set SameSite = (SameSiteMode)(-1)
+                    options.SameSite = SameSiteMode.Unspecified;
+                
+            }
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -39,13 +57,46 @@ namespace DigitalEdu
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
-
-            services.AddAuthentication().AddGoogle(googleOptions =>
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            }).AddCookie(opt =>
+            {
+                opt.Cookie.Name = "TryingoutGoogleOAuth";
+                opt.LoginPath = "/api/user/GoogleSignIn";
+            }
+            )
+              .AddGoogle(googleOptions =>
             {
                 googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
                 googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                googleOptions.Scope.Add("profile");
+                googleOptions.Events.OnCreatingTicket = context =>
+                {
+                    string picuri = context.User.GetProperty("picture").GetString();
+                    context.Identity.AddClaim(new Claim("picture", picuri));
+                    return Task.CompletedTask;
+                };
+                googleOptions.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+            }).AddFacebook(FacebookAuthenticationOptionsExtensions =>
+            {
+                FacebookAuthenticationOptionsExtensions.AppId = Configuration["Authentication:Facebook:AppId"];
+                FacebookAuthenticationOptionsExtensions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
             });
         }
+
+       
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -59,15 +110,15 @@ namespace DigitalEdu
             {
                 app.UseExceptionHandler("/Error");
             }
-
+            app.UseCookiePolicy();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
