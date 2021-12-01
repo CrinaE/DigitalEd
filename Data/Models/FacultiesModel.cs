2e9a624f95b05faa96ededefc94f44d4d4f9a7c3
AllanT12 <alin.tanasa@student.usv.ro>
2021-12-01 15:36:54 +0200

    delete

--- Data/Models/FacultiesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DigitalEdu.Data.Models
-{
-    public class FacultiesModel
-    {   
-        //IdFaculty PK
-        public int IdFaculty { get; set; }
-        public string FacultyName { get; set; }
-    }
-}
