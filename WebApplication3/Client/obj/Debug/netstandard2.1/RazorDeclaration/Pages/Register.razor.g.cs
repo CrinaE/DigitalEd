59286a2d76ae43c28d0f3c320518f4eb1aa81aad
BrogrammingBrah <petru.uhliuc@student.uaic.ro>
2021-12-10 17:39:50 +0200

    can't login/register if there is no @ in the email field or the password field is empty

--- WebApplication3/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Register.razor.g.cs
+++ WebApplication3/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Register.razor.g.cs
@@ -13,98 +13,98 @@ namespace WebApplication3.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 1 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 2 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 3 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 4 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 5 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 6 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 7 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 8 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 9 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using WebApplication3.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 10 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using WebApplication3.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\_Imports.razor"
+#line 11 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Register.razor"
+#line 2 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\Pages\Register.razor"
 using WebApplication3.Client.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Register.razor"
+#line 4 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\Pages\Register.razor"
 using BlazorInputFile;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Register.razor"
+#line 5 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\Pages\Register.razor"
 using System.IO;
 
 #line default
@@ -119,7 +119,7 @@ using System.IO;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 25 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Register.razor"
+#line 25 "C:\Users\User\Source\Repos\DigitalEd\WebApplication3\Client\Pages\Register.razor"
        
     public string ProfilePicDataUrl { get; set; }
 
@@ -138,7 +138,10 @@ using System.IO;
     public async Task RegisterUser()
     {
         await _registerViewModel.SaveProfile();
-        _navigationManager.NavigateTo("/", true);
+        if (_registerViewModel.EmailAddress.Contains('@') && _registerViewModel.EmailAddress != null)
+            if(_registerViewModel.FirstName!=null && _registerViewModel.LastName!=null)
+                if (_registerViewModel.Password != null)
+                   _navigationManager.NavigateTo("/", true);
     }
 
 
