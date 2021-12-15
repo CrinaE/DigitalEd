a696a0bf57c60b467f8b798e5b83802911bc0fb7
AllanT12 <alin.tanasa@student.usv.ro>
2021-12-15 12:22:15 +0200

    ThisIsIt

--- WebApplication3/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Question.razor.g.cs
+++ WebApplication3/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Question.razor.g.cs
@@ -119,7 +119,7 @@ using WebApplication3.Client.ViewModels;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 53 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Question.razor"
+#line 54 "C:\Users\S2018-438316\Documents\GitHub\DigitalEd\WebApplication3\Client\Pages\Question.razor"
        
     [Parameter]
     public int QId { get; set; }
@@ -135,7 +135,16 @@ using WebApplication3.Client.ViewModels;
         _questionViewModel.Likes--;
         _questionViewModel.Like();
     }
+    public void OnClickAnswer()
+    {
+        _answerViewModel.Likes++;
 
+    }
+    public void DownVoteAnswer()
+    {
+        _answerViewModel.Likes--;
+
+    }
     public void add()
     {
         _answerViewModel.IdQuestion = QId;
